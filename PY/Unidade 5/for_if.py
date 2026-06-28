1b2ff1540b648cc08bff01f2a846c29a2e059955
L1m4z1n <gabrielimalmeida@gmail.com>
2026-06-28 00:12:57 -0300

    PY: For + If

---
+++ PY/Unidade 5/for_if.py
@@ -0,0 +1,17 @@
+'''
+for item in lista:
+    if condicao:
+        faça alguma coisa
+    else:
+        outra coisa
+'''
+
+vendas = [1200, 300, 800, 1500, 1900, 2750, 400, 20, 23, 70, 90, 80, 1100, 999, 900, 880, 870, 50, 1111, 120, 300, 450, 800]
+meta = 1000
+
+qtd_meta = 0
+
+for venda in vendas:
+    if venda >= meta:
+        qtd_meta +=1
+        print(f'{qtd_meta}: {venda}')
