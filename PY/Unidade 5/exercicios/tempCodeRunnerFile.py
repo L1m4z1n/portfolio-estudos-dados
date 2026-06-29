416ca9cbd1ab6aa21bada780968e913380a585c7
L1m4z1n <gabrielimalmeida@gmail.com>
2026-06-29 20:17:48 -0300

    PY: Finalizando exercicios sobre for em PY

--- PY/Unidade 5/exercicios/tempCodeRunnerFile.py
+++ PY/Unidade 5/exercicios/tempCodeRunnerFile.py
@@ -1,13 +1,11 @@
-meta = 10000
-vendas = [
-    ['João', 15000],
-    ['Julia', 27000],
-    ['Marcus', 9900],
-    ['Maria', 3750],
-    ['Ana', 10300],
-    ['Alon', 7870],
-]
+produtos = ['iphone', 'galaxy', 'ipad', 'tv', 'máquina de café', 'kindle', 'geladeira', 'adega', 'notebook dell', 'notebook hp', 'notebook asus', 'microsoft surface', 'webcam', 'caixa de som', 'microfone', 'câmera canon']
+vendas2019 = [558147,712350,573823,405252,718654,531580,973139,892292,422760,154753,887061,438508,237467,489705,328311,591120]
+vendas2020 = [951642,244295,26964,787604,867660,78830,710331,646016,694913,539704,324831,667179,295633,725316,644622,994303]
 
-for vendedor in vendas:
-    if vendedor[1] >= meta:
-        print(f"{vendedor[0]} bateu a meta com {vendedor[1]} em vendas!")
+for i,produto in enumerate(produtos):
+    if vendas2020[i] > vendas2019[i]:
+        crescimento = (vendas2020[i]/vendas2019[i] - 1) * 100
+        print(f"{i}: {produto}: 2019 = {vendas2019[i]}, 2020 = {vendas2020[i]}, com crescimento de {crescimento:.2f}%")
+    else:
+        reducao = (1 - vendas2020[i]/vendas2019[i]) * 100
+        print(f"{i}: {produto}: 2019 = {vendas2019[i]}, 2020 = {vendas2020[i]}, com queda de {reducao:.2f}%")
