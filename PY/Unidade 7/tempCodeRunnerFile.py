c7b0cda6c26076446ac6ed03af22a9058cccf506
L1m4z1n <gabrielimalmeida@gmail.com>
2026-07-20 19:42:09 -0300

    PY: Resolvendo exercicios com tuplas!

--- PY/Unidade 7/tempCodeRunnerFile.py
+++ PY/Unidade 7/tempCodeRunnerFile.py
@@ -1,5 +1,9 @@
-vendas = [1000,2000,300,300,150]
-funcionarios = ['João','Lira','Ana','Maria','Paula']
 
-for i,venda in enumerate(vendas):
-    print(f'{funcionarios[i]} vendeu {venda} unidades!')
+vendas_produtos = [('iphone', 558147, 951642), ('galaxy', 712350, 244295), ('ipad', 573823, 26964), ('tv', 405252, 787604), ('máquina de café', 718654, 867660), ('kindle', 531580, 78830), ('geladeira', 973139, 710331), ('adega', 892292, 646016), ('notebook dell', 422760, 694913), ('notebook hp', 154753, 539704), ('notebook asus', 887061, 324831), ('microsoft surface', 438508, 667179), ('webcam', 237467, 295633), ('caixa de som', 489705, 725316), ('microfone', 328311, 644622), ('câmera canon', 591120, 994303)]
+
+#seu código aqui
+
+for produto,vendas2019,vendas2020 in vendas_produtos:
+   if vendas2020 > vendas2019:
+        crescimento = vendas2020 / vendas2019 - 1
+        print(f"{produto} teve {vendas2019}, {vendas2020}. Crescimento de {crescimento}")
