e159f8be88f3bac25335f9f911c1a325ded7d861
L1m4z1n <gabrielimalmeida@gmail.com>
2026-08-03 08:44:54 -0300

    PY: Argumentos para uma function

---
+++ PY/Unidade 10/arg.py
@@ -0,0 +1,39 @@
+'''
+Quantidades indefinidas de Argumentos
+
+Utilidade:
+Quando você quer permitir uma quantidade indefinida de argumentos , usa o * para isso
+
+Estrutura:
+
+*args ara positional arguments -> argumentos vêm em formato de tupla
+
+def minha_funcao(*args):
+    ...
+
+**kwargs para keyword arguments -> argumentos vêm em formato de dicionario
+
+ def minha_funcao(**kwargs):
+    ...
+
+'''
+
+def minha_soma(*numeros):
+    soma = 0
+    for numero in numeros:
+        soma+= numero
+    return soma
+
+print(minha_soma(10,20,30,40))
+
+def preco_final(preco, **adicionais):
+    #print(adicionais)
+    if 'desconto' in adicionais:
+        preco *= (1-adicionais['desconto'])
+    if 'garantia_extra' in adicionais:
+        preco += adicionais['garantia_extra']
+    if 'imposto' in adicionais:
+        preco *= (1+ adicionais['imposto'])
+    return preco
+
+print(preco_final(1000, desconto=0.1))
