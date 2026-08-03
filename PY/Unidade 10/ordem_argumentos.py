844c168855c3eed25e482833e10a0c07067cf91d
L1m4z1n <gabrielimalmeida@gmail.com>
2026-08-03 08:51:34 -0300

    PY: Ordem dos argumentos

---
+++ PY/Unidade 10/ordem_argumentos.py
@@ -0,0 +1,9 @@
+'''
+Ordem dos argumentos
+
+Estrutura:
+- Sempre os positional arguments vêm antes e depois os keywords arguments
+- Sempre os argumentos individuais vêm antes e depois os "múltiplos"
+
+def minha_funcao(arg1, *args, k = kwarg1, **kwargs):
+'''
