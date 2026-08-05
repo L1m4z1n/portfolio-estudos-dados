2d34fc3a90e6ce38bd510009731e37660956302f
L1m4z1n <gabrielimalmeida@gmail.com>
2026-08-05 08:41:39 -0300

    PY: Mais exercicios sobre funções

--- PY/Unidade 10/exercicios/ex5.py
+++ PY/Unidade 10/exercicios/ex5.py
@@ -80,6 +80,7 @@ print(f"Custo dos galões: {custo_galoes}")
 
 ##### 2. Comprar apenas galões de 3,6 litros: (apenas galoes inteiros)
 
+# Resposta junto com as funções de acima
 
 
 
@@ -107,5 +108,55 @@ Para os 17 litros faltantes precisamos de 5 galões que custam 125 reais no tota
 Podemos ir ao exercício:
 '''
 
+# Pegar a área a ser pintada
+def area_a_ser_pintada():
+    area = int(input('Qual é o tamanho em metros quadrados a ser pintada:'))
+    return area
+#Pegar quantos litros eu vou precisar de tinta
+def litros_necessarios(area):
+    litros = area / 6
+    return litros
+#Calcular quantas latas e quantos galões eu vou precisar
+    #Calcular quantas latas inteiras eu vou precisar
+    #Calcular quantos litros ainda faltam comprar
+    #Calcular quanto custa preencher esses litros que faltam com galão
+    #Calcular quanto custa preencher esses litros que faltam com latas
+    #Escolher a opção mais barata
+
+#Calcular custo total
+
+def calcular_custo(latas,galoes):
+    custo_latas = latas * 80
+    custo_galoes = galoes * 25
+    custo = custo_latas + custo_galoes
+    return custo
 
 
+area = area_a_ser_pintada()
+litros_necessarios = (area)
+latas = 0
+galoes = 0
+
+latas_inteiras = int(litros / 18)
+
+litros_faltam = litros % 18
+
+custo_extra_latas = 1 *80
+
+galoes = litros_faltam / 3.6
+if galoes > int(galoes):
+    galoes = int(galoes) + 1
+
+custo_extra_galoes = galoes *25
+
+if custo_extra_latas < custo_extra_galoes:
+    latas = latas_inteiras + 1
+    galoes = 0
+else:
+    latas = latas_inteiras
+
+custo = calcular_custo(latas,galoes)
+print(f"Litros: {litros}")
+print(f"Latas: {latas}")
+print(f"Galoes: {galoes}")
+print(f"Custo: {custo}")
