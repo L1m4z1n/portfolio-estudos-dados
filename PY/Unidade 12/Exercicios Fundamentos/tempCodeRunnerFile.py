c980fbb6ea0a7b33815edd4fb33be3d891c2791c
L1m4z1n <gabrielimalmeida@gmail.com>
2026-08-13 07:11:42 -0300

    PY: Exercicio de gerador de senha

--- PY/Unidade 12/Exercicios Fundamentos/tempCodeRunnerFile.py
+++ PY/Unidade 12/Exercicios Fundamentos/tempCodeRunnerFile.py
@@ -1,40 +1,24 @@
+import random
 import string
-from collections import Counter
-#print(Counter(['a', 'b', 'a', 'c', 'b', 'a']))
-#print(Counter('abacba'))
+
+def gerar_senha(tamanho):
+    if tamanho < 4:
+        print("O tamanho da senha deve ser de pelo menos 4 caracteres")
+    else:
+        senha = [
+            random.choice(string.ascii_letters),
+            random.choice(string.digits),
+            random.choice(string.punctuation),
+        ]
+
+    possibilidades = "".join([string.ascii_letters, string.digits, string.punctuation])
+    senha.extend(random.choices(possibilidades, k=tamanho-3))
+
+    random.shuffle(senha)
+    return "".join(senha)
 
 
-# Solução Simples
 
-def analisar_texto(texto):
 
-    """
-    Analisa o texto fornecido e calcula a contagem de palavras, a frequencia de palavras 
-    e frequência de letras.
-
-    Parameters
-    ----------
-    texto: str
-        texto a ser analisado
-
-    returns:
-    ----------
-    tuple
-        Contagem de palacras, frequência de palavras, frequência de letras
-    
-    """
-
-    tratamento = str.maketrans("","", string.punctuation)
-    texto_tratado = texto.translate(tratamento)
-    palavras = texto.split()
-    contagem_palavras = len(palavras)
-    frequencia_palavras = Counter(palavras)
-    frequencia_letras = Counter(texto_tratado.lower())
-    return contagem_palavras, frequencia_palavras, frequencia_letras
-
-texto = "Olá mundo! Este é um teste. Olá novamente."
-contagem_palavras, frequencia_palavras, frequencia_letras = analisar_texto(texto)
-
-print(f"Contagem de palavras: {contagem_palavras}")
-print(f"Frequência de palavras: {frequencia_palavras}")
-print(f"Frequência de letras: {frequencia_letras}")
+tamanho = int(input("Digite o comprimento da senha:"))
+print(gerar_senha(tamanho))
